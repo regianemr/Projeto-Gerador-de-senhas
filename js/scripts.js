bc7205259106ad2e3114453259162c5869dd693a
Regiane <regianemelo1995@gmail.com>
2024-06-10 11:01:42 -0300

    iniciando as funcoes para gerar caracteres

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,27 @@
-console.log("teste")
+// Seleção de elementos
+const generatePasswordButton = document.querySelector("#generate-password")
+const generatePasswordElement = document.querySelector("#generated-password")
+
+// Funções 
+const getLetterLowerCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+}
+
+const getLetterUpperCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+}
+
+const getNumber = () => {
+    return Math.floor(Math.random() * 10).toString()
+}
+
+const getSymbol = () => {
+    const symbols = "(){}[]=!@.,/$%&"
+    return symbols[Math.floor(Math.random() * symbols.length)]
+}
+
+
+// Eventos
+generatePasswordButton.addEventListener("click", () => {
+    console.log("teste")
+})
